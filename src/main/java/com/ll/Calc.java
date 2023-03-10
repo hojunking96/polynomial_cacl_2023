a64220f2b1e1cb06c87a1fa40d83c8b1bde21a82
hojunking96 <hojunking96@gmail.com>
2023-03-10 16:40:22 +0900

    숫자 여러개

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,26 +2,17 @@ package com.ll;
 
 public class Calc {
     public static int run(String exp) {
-        boolean needToPlus = exp.contains("+");
-        boolean needToMinus = exp.contains("-");
-        String[] bits = null;
+        exp = exp.replaceAll("- ", "+ -");
 
-        if (needToPlus) {
-            bits = exp.split(" \\+ ");
-        } else if (needToMinus) {
-            bits = exp.split(" \\- ");
+        String[] bits = exp.split(" \\+ ");
+        int sum = 0;
+        for (int i = 0; i < bits.length; i++) {
+            sum += Integer.parseInt(bits[i]);
         }
 
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[1]);
-
-        if (needToPlus) {
-            return a + b;
-        } else if (needToMinus) {
-            return a - b;
-        }
+        return sum;
 
 
-        throw new RuntimeException("올바른 계산식이 아닙니다.");
+//        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
--- src/test/java/com/ll/CalcTests.java
+++ src/test/java/com/ll/CalcTests.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-public class CalcTests {@Test
-@DisplayName("1 + 1 == 2")
-void t1() {
-    assertThat(Calc.run("1 + 1")).isEqualTo(2);
-}
+public class CalcTests {
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
 
     @Test
     @DisplayName("2 + 1 == 3")
@@ -29,19 +30,44 @@ void t1() {
     void t4() {
         assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
     }
+
     @Test
     @DisplayName("2 - 1 == 1")
     void t5() {
         assertThat(Calc.run("2 - 1")).isEqualTo(1); // 구현
     }
+
     @Test
     @DisplayName("3 - 1 == 2")
     void t6() {
         assertThat(Calc.run("3 - 1")).isEqualTo(2); // 구현
     }
+
     @Test
     @DisplayName("100 - 20 == 80")
     void t7() {
         assertThat(Calc.run("100 - 20")).isEqualTo(80); // 구현
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    void t8() {
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")
+    void t9() {
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+    }
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 == -20")
+    void t10() {
+        assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+    }
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+    void t11() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
     }
 }

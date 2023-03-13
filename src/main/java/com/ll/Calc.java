b092ecd2b4dbb043cb3f7b9512148512d13bb00e
hojunking96 <hojunking96@gmail.com>
2023-03-13 16:43:36 +0900

    20 + 10 + 5 * 2 == 40

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,7 +1,12 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Calc {
     public static int run(String exp) {
+        // 단일항이 입력되면 바로 리턴
+        if (!exp.contains(" ")) return Integer.parseInt(exp);
         boolean needToMulti = exp.contains("*");
         boolean needToPlus = exp.contains("+");
 
@@ -9,8 +14,12 @@ public class Calc {
 
         if (needToCompound) {
             String[] bits = exp.split(" \\+ ");
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
 
-            return Integer.parseInt(bits[0]) + run(bits[1]);
+            return run(newExp);
         } else if (needToPlus) {
             exp = exp.replaceAll("- ", "+ -");
 
